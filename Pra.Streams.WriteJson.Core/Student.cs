0ea8fedc02c5d007119db64ca5f8ceacb7e15010
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-11-12 07:36:47 +0100

    Add project files.

---
+++ Pra.Streams.WriteJson.Core/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pra.Streams.WriteJson.Core
+{
+    public class Student
+    {
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string City { get; set; }
+
+        public Student(string lastname, string firstname, string city)
+        {
+            Lastname = lastname;
+            Firstname = firstname;
+            City = city;
+        }
+        public override string ToString()
+        {
+            return $"{Lastname} {Firstname}";
+        }
+    }
+}
---
+++ Pra.Streams.WriteJson.Core/StudentService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Pra.Streams.WriteJson.Core
+{
+    public class StudentService
+    {
+        private List<Student> students;
+
+        public List<Student> Students
+        {
+            get { return students; }
+        }
+
+        public StudentService()
+        {
+            GetStudents();
+        }
+        private void GetStudents()
+        {
+            if (File.Exists(Environment.CurrentDirectory + "\\students.json"))
+                ReadJson();
+            else
+                DoSeeding();
+
+        }
+        private void ReadJson()
+        {
+            var content = File.ReadAllText(Environment.CurrentDirectory + "\\students.json");
+            students = JsonConvert.DeserializeObject<List<Student>>(content);
+
+        }
+        private void DoSeeding()
+        {
+            students = new List<Student>();
+            students.Add(new Student("Janssens", "Jan", "Brugge"));
+            students.Add(new Student("Willems", "Wim", "Brugge"));
+            students.Add(new Student("Kaerels", "Charel", "Brugge"));
+            students.Add(new Student("Taer", "Guy", "Brugge"));
+            students.Add(new Student("Banjo", "Lien", "Brugge"));
+            students.Add(new Student("Tumas", "Marie", "Brugge"));
+        }
+
+        public void WriteJson()
+        {
+            var content = JsonConvert.SerializeObject(students);
+            File.WriteAllText(Environment.CurrentDirectory + "\\students.json", content);
+        }
+    }
+}
